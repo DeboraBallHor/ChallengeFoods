52d366510954acc094d28c169ef96f08a8d55193
DeboraBalleriniHorta <49173075+DeboraBalleriniHorta@users.noreply.github.com>
2019-06-14 18:46:16 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/digitalhousefoods/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.digitalhousefoods.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.digitalhouse.digitalhousefoods.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_login);
+    }
+}
---
+++ app/src/main/java/br/com/digitalhouse/digitalhousefoods/Splash/SplashActivity.java
@@ -0,0 +1,6 @@
+package br.com.digitalhouse.digitalhousefoods.Splash;
+
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+}
